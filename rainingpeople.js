b27ebfb6fa06f75faccf21ea1a5a997052896443
ssumg <37644244+ssumg@users.noreply.github.com>
2018-05-10 16:47:03 +0900

    Update rainingpeople.js

--- rainingpeople.js
+++ rainingpeople.js
@@ -7,9 +7,9 @@ function setup() {
   createCanvas(1198, 1005);
   background = loadImage('assets/amebito.png');
   for(var i = 0; i<42; i++)
-    smallman.push(new People());
+    smallman[i] = new People();
   for(var i = 0; i<9; i++)
-    bigman.push(new People());
+    bigman[i] = new People();
     r = 0;
     g = 25;
     b = 0;
@@ -42,8 +42,8 @@ function People(){
   this.ysize = 200;
   this.xPos = random(0, width-200);
   this.yPos = random(-1000, -100);
-  man[0] = loadImage("assets/man0.png");
-  man[1] = loadImage("assets/man1.png");
+  var man[0] = loadImage("assets/man0.png");
+  var man[1] = loadImage("assets/man1.png");
   
   this.display = function(){
     this.time++;
@@ -52,11 +52,11 @@ function People(){
     else
       this.num = 0;
     for(this.xPos = 40; this.xPos < width - 100; this.xPos+=120){
-      image(man[0], this.xPos, this.yPos);
+      image(man[num], this.xPos, this.yPos);
     }
     this.yPos += 15;
+  }
 }
-};
 function keyPressed() {
     if(keyCode === UP_ARROW){
         for(var i=0; i<42; i++){
